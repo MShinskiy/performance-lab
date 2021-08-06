6c9a8a288c01e53fc15af3a3252ab9d787c8b495
MShinskiy <88272807+MShinskiy@users.noreply.github.com>
2021-08-06 13:34:13 +0300

    Correct algorithm

--- task4/task4.java
+++ task4/task4.java
@@ -14,7 +14,8 @@ public class task4 {
         }
 
         //initialization
-        int sum = 0, dev = 0, avg;
+        int dev;
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
         ArrayList<Integer> nums = new ArrayList<>();
 
         //iterate through the numbers
@@ -23,12 +24,25 @@ public class task4 {
             while ((line = br.readLine()) != null) {
                 int num = Integer.parseInt(line);
                 nums.add(num);
-                sum += num;
+                if(num < min) min = num;
+                if(num > max) max = num;
             }
         }
-        avg = sum/nums.size();
-        //find sum of deviation
-        for (int n : nums) dev += Math.abs(avg - n);
-        System.out.println(dev);
+        //initialize an array of
+        int[] roundNums = new int[max-min + 1];
+
+        //round every number in the list to every number in the range (max-min)
+        for(int i = 0; i < roundNums.length; i++) {
+            dev = 0;
+            //do for every num in list
+            for (int n : nums) dev += Math.abs(i+min - n);
+            roundNums[i] = dev;
+        }
+
+        min = Integer.MAX_VALUE;
+        //find a minimum in the array
+        for (int arrOfNum : roundNums) if (arrOfNum < min) min = arrOfNum;
+
+        System.out.println(min);
     }
 }
